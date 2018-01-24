29737d11cd2ff5c25229037525878589dc87d0c6
Dmitry Figol <git@dmfigol.me>
2018-01-24 02:07:49 +0100

    Add env.py

---
+++ tasks/env.py
@@ -0,0 +1,12 @@
+# Replace YOUR-NAME-HERE in the line below
+LAB_USER = 'YOUR-NAME-HERE'
+
+# Get your Cisco Spark access token from developer.ciscospark.com
+# 1) Login
+# 2) Copy the Access Token from top-right corner portrait icon
+# 3) replace YOUR-ACCESS-TOKEN-HERE in the line below
+LAB_USER_SPARK_TOKEN = 'YOUR-ACCESS-TOKEN-HERE'
+
+LAB_SESSION = 'dCloud DNA Network Programmability Lab v1.4'
+
+SPARK_ROOM_NAME = 'CLEU18 TECSDN-3602 Lab'
